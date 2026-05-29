709978db8b8046fd6a3169499697daa77eacac92
Frederik Aalund <fpa@sbtinstruments.com>
2026-05-29 12:34:23 +0000

    chore: bump private-assets to include the sqlite QCDB
    The very same QC database is now in sqlite format in private-assets.

--- lago/low_level/_lago_constants.py
+++ lago/low_level/_lago_constants.py
@@ -1,4 +1,4 @@
 LAGO_CACHE_DIR_NAME = ".lago_cache"
 
 PRIVATE_ASSETS_GIT_URL = "https://github.com/sbtinstruments/private-assets.git"
-PRIVATE_ASSETS_COMMIT = "753eefd92b7719e25abef0e7350209bfcda5b211"
+PRIVATE_ASSETS_COMMIT = "39e6be4f681356fc3b7aa4e6938e69e22ac6707a"
